002c609c5cdb6314214294155f7a5dd5f4077f6e
spartan31 <krishanyadav31081999@gmail.com>
2023-03-14 21:51:07 +0530

    Removed Splash Lifecycle Observer

--- app/src/main/java/com/example/musicals/views/activity/SplashScreen.kt
+++ app/src/main/java/com/example/musicals/views/activity/SplashScreen.kt
@@ -1,14 +1,13 @@
 package com.example.musicals.views.activity
 
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.content.pm.PackageManager
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import com.example.musicals.R
 import com.example.musicals.databinding.ActivitySplashScreenBinding
-import com.example.musicals.views.observer.SplashScreenObserver
 
 class SplashScreen : AppCompatActivity() {
     lateinit var binding: ActivitySplashScreenBinding
@@ -16,10 +15,37 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        lifecycle.addObserver(SplashScreenObserver(binding, this))
+
+        setAnimation()
+        splashAnimate()
 
     }
 
+    private fun setAnimation() {
+        val anim = AnimationUtils.loadAnimation(this, R.anim.splash_logo_rotate)
+        val anim2 = AnimationUtils.loadAnimation(this, R.anim.splash_logo_inner)
+
+        with(binding) {
+            splashLogo.startAnimation(anim)
+            splashInnerLogo.startAnimation(anim2)
+            appTitleSplash.startAnimation(anim2)
+        }
+    }
+
+    private fun splashAnimate(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            val share = getSharedPreferences("login", AppCompatActivity.MODE_PRIVATE)
+            val isLogin = share.getBoolean("flag", false)
+            if (isLogin) {
+                val intent = Intent(this, HomeScreen::class.java)
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, LoginScreen::class.java)
+                startActivity(intent)
+            }
+            finish()
+        }, 5000)
+    }
 
 
 
